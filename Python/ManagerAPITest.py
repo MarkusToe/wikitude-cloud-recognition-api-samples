3f1925b8a95f2c97a15bbeb6976ca30c11ac0873
Markus Török <markus.toeroek@wikitude.com>
2017-08-07 15:13:08 +0200

    Update Java examples

--- Python/ManagerAPITest.py
+++ Python/ManagerAPITest.py
@@ -23,43 +23,6 @@ EXAMPLE_IMAGE_URLS = [
 try:
     # create a target colection with the name testCollection
     # and store the id which is of the target collection which is returned
-    print '\n\nCREATE TARGETCOLLECTION:'
-    result = api.createTargetCollection('testCollection')
-    print result
-    tcId = result['id']
-
-    # add a target to the target collection which was created in the previous step
-    print '\n\nADD TARGET:'
-    target = {
-        "name": "TC1",
-        "imageUrl": EXAMPLE_IMAGE_URLS[0]
-    }
-    result = api.addTarget(tcId, target)
-    targetId = result['id']
-    print 'added target: {0}'.format(targetId)
-
-    # add multiple targets to the target collection
-    print '\n\nADD TARGETS:'
-    targets = [{
-        "name": "TC2",
-        "imageUrl": EXAMPLE_IMAGE_URLS[1]
-    }]
-    print api.addTargets(tcId, targets)
-
-    # publish the target collection. After the target collection has been published it can be used for recognition
-    print '\n\nPUBLISH TARGETCOLLECTION:'
-    print api.generateTargetCollection(tcId)
-
-    # clean up
-    print '\n\nDELETE TARGETCOLLECTION:'
-    api.deleteTargetCollection(tcId)
-    print 'deleted target collection: {0}'.format(tcId)
-except APIException as e:
-    print e
-
-try:
-    # create a target colection with the name testCollection
-    # and store the id which is of the target collection which is returned
     print '\n\nCreate Image Target Collection:'
     result = api.createTargetCollection('testCollection')
     print result
